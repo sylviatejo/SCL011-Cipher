659500e3b27d056801213071152258020bd2c7ac
sylviatejo <sylvia.tejo@gmail.com>
2019-08-23 14:30:04 -0300

    Tengo listo el código, test y css

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,56 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (message, offset) => {
+
+//Declaro el BUCLE FOR PARA CIFRADO
+
+let outterMessage = ""; //Le doy el espacio en cadena a lo que se tiene que imprimir con innertHTML
+
+for (let i = 0; i < message.length; i++){ /* Bucle inicia en 0 por ser posición inicial de A. Mientras sea menor al mensaje,
+    se continúa ejecutando. Recorre de uno en uno las letras.*/
     
+    if (message.charCodeAt(i)>=65 && message.charCodeAt(i)<=90) {
+
+     let asciiNum = message.charCodeAt(i); //Variable que entrega el valor del mensaje en ASCII 
+     let cipher = ((parseInt(asciiNum) - 65 + offset) % 26 + 65); /* Entrega los valores ASCII CIFRADOS con offset. Parseint se aplica sobre asciiNum para transformarlos en números */
+     let cipherAbc = String.fromCharCode(cipher); //Pasa el mensaje Cifrado ASCII a Abecedario Normal.
+     outterMessage+=cipherAbc //Concatena lo que toma de cipherAbc para imprimirlo en outText
+    //outText.innerHTML=outterMessage; // Imprimo el resultado de cipherABC en outText
+    }
+    else if (message.charCodeAt(i)===32) {
+
+        let asciiNum = message.charCodeAt(i)
+        let newStr = String.fromCharCode(asciiNum);
+        outterMessage+=newStr
+        //outText.innerHTML=outterMessage;
+      }
+  }
+    return outterMessage
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (message, offset) => {
+
+//Declaro el BUCLE FOR PARA DECIFRADO
+
+let outterMessage = ""; //Le doy el espacio en cadena a lo que se tiene que imprimir con innertHTML
+
+for (let x = 0; x < message.length; x++){ /* Bucle inicia en 0 por ser posición inicial de A. Mientras sea menor al mensaje,
+    se continúa ejecutando. Recorre de uno en uno las letras.*/
+    
+    if (message.charCodeAt(x)>=65 && message.charCodeAt(x)<=90) {
+
+    let asciiNumD = message.charCodeAt(x); //Variable que entrega el valor del mensaje en ASCII CIFRADO
+     let deCipher = ((parseInt(asciiNumD) + 65 - offset) % 26 + 65); /* Entrega los valores ASCII CIFRADOS con offset. Parseint se aplica sobre asciiNum para transformarlos en números */
+     let cipherAbcD = String.fromCharCode(deCipher); //Pasa el mensaje Cifrado ASCII a Abecedario Normal.
+     outterMessage+=cipherAbcD //Concatena lo que toma de cipherAbcD para imprimirlo en outText
+     //outText.innerHTML=outterMessage; // Imprimo el resultado de cipherAbcD en outText
+    }
+    else if (message.charCodeAt(x)===32) {
+
+        let asciiNumD = message.charCodeAt(x)
+        let newStr = String.fromCharCode(asciiNumD);
+        outterMessage+=newStr
+        //outText.innerHTML=outterMessage;
+      }
+  }
+  return outterMessage;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-/*Acá va tu código*/
+let message // Declaro variable que guarda el valor que se ingrese en el textarea
+let offset ;// Declaro variable que guarda el número que ingrese el usuario
+const outText = document.getElementById("outText")
+
+//Comienzo por darle utilidad al Botón "ENVIAR CLAVE"
+
+document.getElementById("sendOffset").addEventListener("click", /* Le digo al documento que el botón ENVIAR CLAVE va a escuchar un
+//evento y que al hacer click se ejecutará mi función */
+() => {
+    offset = Number.parseInt(document.getElementById("offsetNumber").value);
+     //Función que guarda el valor de offset.
+})
+
+//Doy utilidad al Botón "CIFRAR"
+
+document.getElementById("code").addEventListener("click", /* Le digo al documento que el botón CIFRAR va a escuchar un
+evento y que al hacer click se ejecutará mi función */
+() => {
+    message = document.getElementById("inText").value
+    outText.innerHTML = window.cipher.encode(message,offset)
+}); //Función que guarda el valor de message.
+
+//Doy utilidad al Botón "DECIFRAR"
+
+document.getElementById("decode").addEventListener("click", /* Le digo al documento que el botón DECIFRAR va a escuchar un
+evento y que al hacer click se ejecutará mi función */
+() => {
+    
+    message = document.getElementById("inText").value //Función que guarda el valor de message.
+    outText.innerHTML = window.cipher.decode(message,offset)
+    
+})
+    
